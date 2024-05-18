726efe76f95509d750d76a183b9202e4b007f226
Yua2 <hero_kanenyu_bynx2maru@yahoo.co.jp>
2024-05-18 21:09:14 +0900

    revalidateを300に変更

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -11,7 +11,7 @@ export const fetchAllBlogs = async (): Promise<BlogType[]> => {
     endpoint: "blogs",
     customRequestInit: {
       next: {
-        revalidate: 60,
+        revalidate: 300,
       },
     },
   });
