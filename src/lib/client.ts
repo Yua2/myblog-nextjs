f4aec3ce8bae4f00595592fbea3fd7675973985f
Yua2 <hero_kanenyu_bynx2maru@yahoo.co.jp>
2024-05-18 22:47:58 +0900

    no-storeを再追加

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -22,6 +22,9 @@ export const fetchDetailBlog = async (contentId: string): Promise<BlogType> => {
   const detailBlog = await client.getListDetail<BlogType>({
     endpoint: "blogs",
     contentId: contentId,
+    customRequestInit: {
+      cache: "no-store",
+    },
   });
   return detailBlog;
 };
