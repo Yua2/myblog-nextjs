21636a8eae076c1cc2c3bf5de5d9bfd3d24ed02a
Yua2 <hero_kanenyu_bynx2maru@yahoo.co.jp>
2024-05-19 20:12:47 +0900

    Linkを使った画面遷移に修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,24 +4,25 @@ import Toolbar from "@mui/material/Toolbar";
 import { Box, IconButton, Typography } from "@mui/material";
 import { HomeOutlined } from "@mui/icons-material";
 import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar>
         <Box flexGrow={1} />
-        <IconButton color="inherit" href="/" sx={{ flexDirection: "column" }}>
-          <HomeOutlined />
-          <Typography variant="caption">トップ</Typography>
-        </IconButton>
-        <IconButton
-          color="inherit"
-          href="/profile"
-          sx={{ flexDirection: "column" }}
-        >
-          <Person2OutlinedIcon />
-          <Typography variant="caption">プロフィール</Typography>
-        </IconButton>
+        <Link href="/">
+          <IconButton color="inherit" sx={{ flexDirection: "column" }}>
+            <HomeOutlined />
+            <Typography variant="caption">トップ</Typography>
+          </IconButton>
+        </Link>
+        <Link href="/profile">
+          <IconButton color="inherit" sx={{ flexDirection: "column" }}>
+            <Person2OutlinedIcon />
+            <Typography variant="caption">プロフィール</Typography>
+          </IconButton>
+        </Link>
       </Toolbar>
     </AppBar>
   );
